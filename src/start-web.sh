0b8647f5873da0f697d9a009960a0aafafa0b375
Florin <florin@makehappen.com>
2019-05-01 21:46:01 -0700

    improve performance using cached and delegated

--- src/start-web.sh
+++ src/start-web.sh
@@ -11,8 +11,8 @@ docker run \
 --detach \
 --name=server_container \
 --publish 80:80 \
---volume=$PWD/localhost/www:/var/www \
---volume=$PWD/localhost/log/apache2:/var/log/apache2 \
+--volume=$PWD/localhost/www:/var/www:cached \
+--volume=$PWD/localhost/log/apache2:/var/log/apache2:delegated \
 --link=mysql_container:mysql_container \
 --link=redis_container:redis_container \
 --volume=$PWD/.ssh:/root/.ssh \
