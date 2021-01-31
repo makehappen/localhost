7aea34a7a27e06bf2038941db0e47b97be87be55
Florin <florin@makehappen.com>
2021-01-30 19:56:37 -0700

    image publish path fix

--- src/docker/php7.4/publish.sh
+++ src/docker/php7.4/publish.sh
@@ -4,8 +4,8 @@ after5
 # pass
 
 # PUBLISH DOCKER IMAGE
-cd ~/localhost/repo/src/docker/ubuntu20.04-apache2-php7.4/
-docker build -t ubuntu16.04-apache-php7.1 .
+cd ~/localhost/repo/src/docker/php7.4/ubuntu20.04-apache2-php7.4/
+docker build -t ubuntu16.04-apache-php7.4 .
 docker images
 # tag the Image ID for "web-server"
 docker tag [IMAGE-ID] after5/ubuntu20.04-apache2-php7.4:latest
